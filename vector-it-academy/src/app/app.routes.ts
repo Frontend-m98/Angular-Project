7f36485f7165c3264d5a6bf75be1fd43516638bf
Frontend-m98 <azizqulovmuhiddin466@gmail.com>
2025-09-12 17:07:50 +0500

    add 404 page and notes

--- vector-it-academy/src/app/app.routes.ts
+++ vector-it-academy/src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.components';
 import { StudentsComponent } from './students/students.components';
 import { MentorComponent } from './mentor/mentor.components';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: 'admin', component: AdminComponent },
     { path: 'students', component: StudentsComponent },
     { path: 'mentor', component: MentorComponent },
+    { path: "not-found", component: NotFoundComponent },
+    { path: "**", redirectTo: 'not-found'},
 ];
